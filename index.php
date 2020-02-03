f53017fcbe5f012610c84b5788c0f60ffd5f2ef8
mathewjmartin <matthewjmartin@zoho.com>
2020-02-03 16:22:58 +0000

    Added b/g image to page

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html lang="en-gb">
+
+<head>
+    <link rel="stylesheet" type="text/css" href="normalize.css" />
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+    <title>Dragon's Collection</title>
+    <body>
+    <h2>Dragon’s Race to the Edge Characters</h2>
 
 
 
+    </body>
+</head>
