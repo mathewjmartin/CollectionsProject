f538c317b10a8c9178c8cbea4a88a99e11fd435c
mathewjmartin <matthewjmartin@zoho.com>
2020-02-04 10:03:34 +0000

    Updated HTML with images

--- index.php
+++ index.php
@@ -4,11 +4,110 @@
 <head>
     <link rel="stylesheet" type="text/css" href="normalize.css" />
     <link rel="stylesheet" type="text/css" href="styles.css" />
+    <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet" />
     <title>Dragon's Collection</title>
-    <body>
-    <h2>Dragon’s Race to the Edge Characters</h2>
+</head>
 
+        <div class="header">
+            <h2>Dragon’s Race to the Edge Characters</h2>
+        </div>
 
+        <div class="dragonContainer">
+            <div class="dragonBox">
+                <img class="dragonImage" src="Images/Toothless.jpg">
+                <h3 class="dragonName">Toothless</h3>
+                <ul>
+                    <li>Rider: Hiccup</li>
+                    <li>Colour: Black</li>
+                    <li>Attack: 15</li>
+                    <li>Speed: 20</li>
+                    <li>Firepower: 14</li>
+                </ul>
+            </div>
+
+            <div class="dragonBox">
+                <img class="dragonImage" src="Images/hookfang.jpg">
+                <h3 class="dragonName">Hookfang</h3>
+                <ul>
+                    <li>Rider: Snotlout</li>
+                    <li>Colour: Red</li>
+                    <li>Attack: 50</li>
+                    <li>Speed: 25</li>
+                    <li>Firepower: 18</li>
+                </ul>
+            </div>
+
+            <div class="dragonBox">
+                <img class="dragonImage" src="Images/meatlug.jpg">
+                <h3 class="dragonName">Meatlug</h3>
+                <ul>
+                    <li>Rider: Fishlegs</li>
+                    <li>Colour: Brown</li>
+                    <li>Attack: 19</li>
+                    <li>Speed: 30</li>
+                    <li>Firepower: 17</li>
+                </ul>
+            </div>
+
+            <div class="dragonBox">
+                <img class="dragonImage" src="Images/stormfly.jpg">
+                <h3 class="dragonName">Stormfly</h3>
+                <ul>
+                    <li>Rider: Astrid</li>
+                    <li>Colour: Blue</li>
+                    <li>Attack: 25</li>
+                    <li>Speed: 14</li>
+                    <li>Firepower: 20</li>
+                </ul>
+            </div>
+
+            <div class="dragonBox">
+                <img class="dragonImage" src="Images/Toothless.jpg">
+                <h3 class="dragonName">Toothless</h3>
+                <ul>
+                    <li>Rider: </li>
+                    <li>Colour: </li>
+                    <li>Attack: </li>
+                    <li>Speed: </li>
+                    <li>Firepower: </li>
+                </ul>
+            </div>
+
+            <div class="dragonBox">
+                <img class="dragonImage" src="Images/hookfang.jpg">
+                <h3 class="dragonName">Toothless</h3>
+                <ul>
+                    <li>Rider: </li>
+                    <li>Colour: </li>
+                    <li>Attack: </li>
+                    <li>Speed: </li>
+                    <li>Firepower: </li>
+                </ul>
+            </div>
+
+            <div class="dragonBox">
+                <img class="dragonImage" src="Images/Toothless.jpg">
+                <h3 class="dragonName">Toothless</h3>
+                <ul>
+                    <li>Rider: </li>
+                    <li>Colour: </li>
+                    <li>Attack: </li>
+                    <li>Speed: </li>
+                    <li>Firepower: </li>
+                </ul>
+            </div>
+
+            <div class="dragonTile">
+                <img class="dragonImage" src="Images/Toothless.jpg">
+                <h3 class="dragonName">Toothless</h3>
+                <ul>
+                    <li>Rider: </li>
+                    <li>Colour: </li>
+                    <li>Attack: </li>
+                    <li>Speed: </li>
+                    <li>Firepower: </li>
+                </ul>
+            </div>
+        </div>
 
     </body>
-</head>
