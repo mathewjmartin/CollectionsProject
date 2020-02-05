f22bfb752441d130836b10a8ddb5707a693ca37e
mathewjmartin <matthewjmartin@zoho.com>
2020-02-05 13:30:28 +0000

    removed html, added php

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
 
-require_once 'functions.php';
 require_once 'dbconnect.php';
-$db=db();
+require_once 'functions.php';
+
+$db = db();
+
+$dragons = getAllDragons($db);
+$dragonHTML= displayDragon($dragons);
+
+//var_dump($dragons);
 
 ?>
 
@@ -25,111 +31,11 @@ $db=db();
 
         <section class="dragonContainer">
 
-            <div class="outerContainer">
-            <div class="dragonBox">
-                <img class="dragonImage" src="Images/Toothless.jpg">
-                <h3 class="dragonName">Toothless</h3>
-                <ul>
-                    <li>Rider: Hiccup</li>
-                    <li>Colour: Black</li>
-                    <li>Attack: 15</li>
-                    <li>Speed: 30</li>
-                    <li>Firepower: 14</li>
-                </ul>
-            </div>
-            </div>
+            <?php
 
-            <div class="outerContainer">
-            <div class="dragonBox">
-                <img class="dragonImage" src="Images/hookfang.jpg">
-                <h3 class="dragonName">Hookfang</h3>
-                <ul>
-                    <li>Rider: Snotlout</li>
-                    <li>Colour: Red</li>
-                    <li>Attack: 50</li>
-                    <li>Speed: 25</li>
-                    <li>Firepower: 18</li>
-                </ul>
-            </div>
-            </div>
+            echo $dragonHTML;
 
-            <div class="outerContainer">
-            <div class="dragonBox">
-                <img class="dragonImage" src="Images/meatlug.jpg">
-                <h3 class="dragonName">Meatlug</h3>
-                <ul>
-                    <li>Rider: Fishlegs</li>
-                    <li>Colour: Brown</li>
-                    <li>Attack: 19</li>
-                    <li>Speed: 20</li>
-                    <li>Firepower: 17</li>
-                </ul>
-            </div>
-            </div>
-
-            <div class="outerContainer">
-            <div class="dragonBox">
-                <img class="dragonImage" src="Images/stormfly.jpg">
-                <h3 class="dragonName">Stormfly</h3>
-                <ul>
-                    <li>Rider: Astrid</li>
-                    <li>Colour: Blue</li>
-                    <li>Attack: 25</li>
-                    <li>Speed: 14</li>
-                    <li>Firepower: 20</li>
-                </ul>
-            </div>
-            </div>
-
-
-
-<!--            <div class="dragonBox">-->
-<!--                <img class="dragonImage" src="Images/Toothless.jpg">-->
-<!--                <h3 class="dragonName">Toothless</h3>-->
-<!--                <ul>-->
-<!--                    <li>Rider: </li>-->
-<!--                    <li>Colour: </li>-->
-<!--                    <li>Attack: </li>-->
-<!--                    <li>Speed: </li>-->
-<!--                    <li>Firepower: </li>-->
-<!--                </ul>-->
-<!--            </div>-->
-<!---->
-<!--            <div class="dragonBox">-->
-<!--                <img class="dragonImage" src="Images/hookfang.jpg">-->
-<!--                <h3 class="dragonName">Toothless</h3>-->
-<!--                <ul>-->
-<!--                    <li>Rider: </li>-->
-<!--                    <li>Colour: </li>-->
-<!--                    <li>Attack: </li>-->
-<!--                    <li>Speed: </li>-->
-<!--                    <li>Firepower: </li>-->
-<!--                </ul>-->
-<!--            </div>-->
-<!---->
-<!--            <div class="dragonBox">-->
-<!--                <img class="dragonImage" src="Images/Toothless.jpg">-->
-<!--                <h3 class="dragonName">Toothless</h3>-->
-<!--                <ul>-->
-<!--                    <li>Rider: </li>-->
-<!--                    <li>Colour: </li>-->
-<!--                    <li>Attack: </li>-->
-<!--                    <li>Speed: </li>-->
-<!--                    <li>Firepower: </li>-->
-<!--                </ul>-->
-<!--            </div>-->
-<!---->
-<!--            <div class="dragonTile">-->
-<!--                <img class="dragonImage" src="Images/Toothless.jpg">-->
-<!--                <h3 class="dragonName">Toothless</h3>-->
-<!--                <ul>-->
-<!--                    <li>Rider: </li>-->
-<!--                    <li>Colour: </li>-->
-<!--                    <li>Attack: </li>-->
-<!--                    <li>Speed: </li>-->
-<!--                    <li>Firepower: </li>-->
-<!--                </ul>-->
-<!--            </div>-->
+            ?>
 
 
         </section>
