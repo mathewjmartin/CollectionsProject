056c4b2a756349828459f8f32c017aa953c1f2d6
mathewjmartin <matthewjmartin@zoho.com>
2020-02-05 13:29:30 +0000

    foreach loop added

--- functions.php
+++ functions.php
@@ -1,13 +1,34 @@
 <?php
 
 function getAllDragons($db) {
-    $query = $db->prepare("SELECT `id`, `name`, `rider`, `attack`, `speed`, `firepower`, `images`
+    $query = $db->prepare("SELECT `id`, `name`, `colour`, `rider`, `attack`, `speed`, `firepower`, `images`
     FROM `dragons`;");
     $query->execute();
-    $result = $query->fetchAll();
-    return $result;
+    return $query->fetchAll();
+}
+
+function displayDragon($dragons) {
+    $output = "";
+    foreach($dragons as $dragon) {
+        $output .= "<div class='outerContainer'>
+                        <div class='dragonBox'>
+                            <img class='dragonImage' src='" . $dragon['images'] . "'>
+                            <h3 class='dragonName'>" . $dragon['name'] . "</h3>
+                            <ul>
+                                <li>Rider: " . $dragon['rider'] . "</li>
+                                <li>Colour: " . $dragon['colour'] . "</li>
+                                <li>Attack: " . $dragon['attack'] . "</li>
+                                <li>Speed: " . $dragon['speed'] . "</li>
+                                <li>Firepower: " . $dragon['firepower'] . "</li>
+                            </ul>
+                        </div>
+                    </div>";
+    }
+    return $output;
+
+    var_dump($dragon);
+
+
 }
 
 
-
-?>
