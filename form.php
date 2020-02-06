9d758dff4ec8f676be750d7ef7d53390a9320891
mathewjmartin <matthewjmartin@zoho.com>
2020-02-06 10:54:02 +0000

    upload form.php

---
+++ form.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en-gb">
+
+<head>
+    <link rel="stylesheet" type="text/css" href="normalize.css" />
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+    <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet" />
+    <meta name="viewport" content="width=device-width">
+    <title>Add a Dragon</title>
+</head>
+
+<body>
+
+    <div class="header">
+        <h2>Add a Dragon</h2>
+    </div>
+
+    <div class="addDragonContainer">
+            <form action="index.php">
+                Name:
+                <input type="text" name="name" value="">
+                <br><br>
+                Rider:
+                <input type="text" name="rider" value="">
+                <br><br>
+                Colour:
+                <input type="text" name="colour" value="">
+                <br><br>
+                Attack:
+                <input type="text" name="attack" value="">
+                <br><br>
+                Speed:
+                <input type="text" name="speed" value="">
+                <br><br>
+                Firepower:
+                <input type="text" name="firepower" value="">
+                <br><br>
+                Image:
+                <input type="text" name="image" value="">
+                <br><br>
+                <input type="submit" value="Submit">
+            </form>
+    </div>
+
+    <h2>Back to the dragons!</h2>
+
+
+
+</body>
+</html>
+
+
+
+</section>
+<div class="dragonButton">
+    <button>Add a Dragon >></button>
+</div>
