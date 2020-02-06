7c164bcaa4bf250724a4882a4cae486927296caf
mathewjmartin <matthewjmartin@zoho.com>
2020-02-06 10:49:16 +0000

    added docblocks

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,11 @@
 <?php
 
-function db() {
+/**
+ * Connection to db
+ *
+ * @return PDO the db connection
+ */
+function db(): PDO {
     $db = new PDO('mysql:host=db; dbname=matt_collection', 'root', 'password');
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     return $db;
