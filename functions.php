30305e6a25fa4faa14a9e06727d5e3b0e9f49fc2
mathewjmartin <matthewjmartin@zoho.com>
2020-02-06 10:26:53 +0000

    reuploaded deleted files

---
+++ functions.php
@@ -0,0 +1,31 @@
+<?php
+
+function getAllDragons($db) {
+    $query = $db->prepare("SELECT `id`, `name`, `colour`, `rider`, `attack`, `speed`, `firepower`, `images`
+    FROM `dragons`;");
+    $query->execute();
+    return $query->fetchAll();
+}
+
+function displayDragon(array $dragons): string {
+    $output = "";
+    foreach($dragons as $dragon) {
+        $output .= "<div class='outerContainer'>
+                        <div class='dragonBox'>
+                            <img class='dragonImage' src='" . $dragon['images'] . "'>
+                            <h3 class='dragonName'>" . $dragon['name'] . "</h3>
+                            <ul>
+                                <li>Rider: " . $dragon['rider'] . "</li>
+                                <li>Colour: " . $dragon['colour'] . "</li>
+                                <li>Attack: " . $dragon['attack'] . "</li>
+                                <li>Speed: " . $dragon['speed'] . "</li>
+                                <li>Firepower: " . $dragon['firepower'] . "</li>
+                            </ul>
+                        </div>
+                    </div>";
+    }
+    return $output;
+
+
+
+}
--- index.php
+++ index.php
@@ -1,4 +1,43 @@
 <?php
 
+require_once "dbconnect.php";
+require_once "functions.php";
+
+$db = db();
+$dragons = getAllDragons($db);
+$dragonHTML = displayDragon($dragons);
 
 
+?>
+
+
+<!DOCTYPE html>
+<html lang="en-gb">
+
+<head>
+    <link rel="stylesheet" type="text/css" href="normalize.css" />
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+    <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet" />
+    <meta name="viewport" content="width=device-width">
+    <title>Dragon's Collection</title>
+</head>
+
+<body>
+        <div class="header">
+            <h2>Dragon’s Race to the Edge Characters</h2>
+        </div>
+
+        <section class="dragonContainer">
+
+            <?php
+
+            echo $dragonHTML;
+
+            ?>
+
+
+
+
+
+</body>
+</html>
