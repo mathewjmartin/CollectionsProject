28f03fcb5e9ac856b65eb12b230cfefd8fa02475
mathewjmartin <matthewjmartin@zoho.com>
2020-02-06 13:21:33 +0000

    updated main page with button

--- index.php
+++ index.php
@@ -35,7 +35,11 @@ $dragonHTML = displayDragon($dragons);
 
             ?>
 
-
+        </section>
+        
+        <div class="dragonButton">
+            <button>Add a Dragon >></button>
+        </div>
 
 
 
